b7a3bc800d88dae795e72b235f779b202fb789d5
ajaychaudhary7 <chaudharya345@gmail.com>
2023-11-21 21:42:01 +0100

    updated for transforms and optimized dataset

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,6 +1,11 @@
 import os
 import subprocess
 import zipfile
+import pandas as pd
+import sqlite3
+from time import time
+import numpy as np
+import gc
 
 def download_kaggle_competition_data(competition_name, output_directory):
     competition_command = f"kaggle competitions download -c {competition_name} -p {output_directory}"
@@ -10,10 +15,145 @@ def extract_zip(zip_path, output_directory):
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(output_directory)
 
+def convert_to_db(db_file, data_frame, table_name):
+    t1 = time()
+    print("SQLite DB Operations....")
+    conn = sqlite3.connect(db_file)
+    data_frame.to_sql(table_name, conn, if_exists='replace', index=False)
+    conn.close()
+    t2 = time()
+    print("Finish: Data Loading  {} s ".format(t2 - t1))
+
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage(deep=True).sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage(deep=True).sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
+def load_and_transform(df,train=True):
+    df = reduce_mem_usage(df)
+    if train:
+        good_cols = list(df.columns)
+        for col in df.columns:
+            rate = df[col].value_counts(normalize=True, dropna=False).values[0]
+            if rate > 0.9:
+                good_cols.remove(col)
+        df = df[good_cols]
+        return df, good_cols
+    return df
+
 def delete_zip(zip_path):
     os.remove(zip_path)
 
 if __name__ == "__main__":
+    dtypes = {
+        'MachineIdentifier':                                    'category',
+        'ProductName':                                          'category',
+        'EngineVersion':                                        'category',
+        'AppVersion':                                           'category',
+        'AvSigVersion':                                         'category',
+        'IsBeta':                                               'int8',
+        'RtpStateBitfield':                                     'float16',
+        'IsSxsPassiveMode':                                     'int8',
+        'DefaultBrowsersIdentifier':                            'float16',
+        'AVProductStatesIdentifier':                            'float32',
+        'AVProductsInstalled':                                  'float16',
+        'AVProductsEnabled':                                    'float16',
+        'HasTpm':                                               'int8',
+        'CountryIdentifier':                                    'int16',
+        'CityIdentifier':                                       'float32',
+        'OrganizationIdentifier':                               'float16',
+        'GeoNameIdentifier':                                    'float16',
+        'LocaleEnglishNameIdentifier':                          'int8',
+        'Platform':                                             'category',
+        'Processor':                                            'category',
+        'OsVer':                                                'category',
+        'OsBuild':                                              'int16',
+        'OsSuite':                                              'int16',
+        'OsPlatformSubRelease':                                 'category',
+        'OsBuildLab':                                           'category',
+        'SkuEdition':                                           'category',
+        'IsProtected':                                          'float16',
+        'AutoSampleOptIn':                                      'int8',
+        'PuaMode':                                              'category',
+        'SMode':                                                'float16',
+        'IeVerIdentifier':                                      'float16',
+        'SmartScreen':                                          'category',
+        'Firewall':                                             'float16',
+        'UacLuaenable':                                         'float32',
+        'Census_MDC2FormFactor':                                'category',
+        'Census_DeviceFamily':                                  'category',
+        'Census_OEMNameIdentifier':                             'float16',
+        'Census_OEMModelIdentifier':                            'float32',
+        'Census_ProcessorCoreCount':                            'float16',
+        'Census_ProcessorManufacturerIdentifier':               'float16',
+        'Census_ProcessorModelIdentifier':                      'float16',
+        'Census_ProcessorClass':                                'category',
+        'Census_PrimaryDiskTotalCapacity':                      'float32',
+        'Census_PrimaryDiskTypeName':                           'category',
+        'Census_SystemVolumeTotalCapacity':                     'float32',
+        'Census_HasOpticalDiskDrive':                           'int8',
+        'Census_TotalPhysicalRAM':                              'float32',
+        'Census_ChassisTypeName':                               'category',
+        'Census_InternalPrimaryDiagonalDisplaySizeInInches':    'float16',
+        'Census_InternalPrimaryDisplayResolutionHorizontal':    'float16',
+        'Census_InternalPrimaryDisplayResolutionVertical':      'float16',
+        'Census_PowerPlatformRoleName':                         'category',
+        'Census_InternalBatteryType':                           'category',
+        'Census_InternalBatteryNumberOfCharges':                'float32',
+        'Census_OSVersion':                                     'category',
+        'Census_OSArchitecture':                                'category',
+        'Census_OSBranch':                                      'category',
+        'Census_OSBuildNumber':                                 'int16',
+        'Census_OSBuildRevision':                               'int32',
+        'Census_OSEdition':                                     'category',
+        'Census_OSSkuName':                                     'category',
+        'Census_OSInstallTypeName':                             'category',
+        'Census_OSInstallLanguageIdentifier':                   'float16',
+        'Census_OSUILocaleIdentifier':                          'int16',
+        'Census_OSWUAutoUpdateOptionsName':                     'category',
+        'Census_IsPortableOperatingSystem':                     'int8',
+        'Census_GenuineStateName':                              'category',
+        'Census_ActivationChannel':                             'category',
+        'Census_IsFlightingInternal':                           'float16',
+        'Census_IsFlightsDisabled':                             'float16',
+        'Census_FlightRing':                                    'category',
+        'Census_ThresholdOptIn':                                'float16',
+        'Census_FirmwareManufacturerIdentifier':                'float16',
+        'Census_FirmwareVersionIdentifier':                     'float32',
+        'Census_IsSecureBootEnabled':                           'int8',
+        'Census_IsWIMBootEnabled':                              'float16',
+        'Census_IsVirtualDevice':                               'float16',
+        'Census_IsTouchEnabled':                                'int8',
+        'Census_IsPenCapable':                                  'int8',
+        'Census_IsAlwaysOnAlwaysConnectedCapable':              'float16',
+        'Wdft_IsGamer':                                         'float16',
+        'Wdft_RegionIdentifier':                                'float16',
+        'HasDetections':                                        'int8'
+        }
+
     kaggle_competition_name = "microsoft-malware-prediction"
     output_directory = "../data/"
     
@@ -28,4 +168,16 @@ if __name__ == "__main__":
     print("Output directory after extracting:", os.path.abspath(output_directory))
     
     delete_zip(zip_filename)
-    print("Competition data downloaded and extracted successfully.")
+    train_df = pd.read_csv(os.path.join(os.path.abspath(output_directory),'train.csv'),low_memory=False)
+    train_df, good_cols = load_and_transform(train_df,train=True)
+    convert_to_db(os.path.join(os.path.abspath(output_directory),'train.sqlite'),train_df,'train')
+    del train_df
+    gc.collect()
+    test_dtypes = {k: v for k, v in dtypes.items() if k in good_cols}
+    test_df = pd.read_csv(os.path.join(os.path.abspath(output_directory),'test.csv'),usecols=good_cols[:-1],low_memory=False)
+    test_df = load_and_transform(test_df,train=False)
+    convert_to_db(os.path.join(os.path.abspath(output_directory),'test.sqlite'),test_df,'test')
+    del test_df
+    gc.collect()
+
+    print("Competition data downloaded and transformed successfully.")
